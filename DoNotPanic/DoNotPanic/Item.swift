4923cb90c1e631f0f5ea74c4ce7327afd209bd91
837100 <no8371@gmail.com>
2025-04-08 17:35:50 +0900

    Item 모델에 속성 추가 및 초기화 메서드 수정

--- DoNotPanic/DoNotPanic/Item.swift
+++ DoNotPanic/DoNotPanic/Item.swift
@@ -6,13 +6,26 @@
 //
 
 import Foundation
+import MapKit
 import SwiftData
 
 @Model
 final class Item {
+    @Attribute(.unique) var id: UUID
+    @Attribute(.externalStorage) var photoData: Data?
+    var accidentTime: Date
+    var locationDetail: String
     var timestamp: Date
+    var latitude: Double?
+    var longitude: Double?
     
-    init(timestamp: Date) {
-        self.timestamp = timestamp
+    init?(photoData: Data?, accidentTime: Date, locationDetail: String, latitude: Double?, longitude: Double?) {
+        self.id = UUID()
+        self.photoData = photoData
+        self.accidentTime = accidentTime
+        self.locationDetail = locationDetail
+        self.latitude = latitude
+        self.longitude = longitude
+        self.timestamp = Date()
     }
 }
